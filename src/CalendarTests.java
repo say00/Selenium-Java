e2363fb8e3dbdf3c86827413fcae24bacc2767eb
jovanmilosevic <jovanmil87@gmail.com>
2023-11-26 21:42:44 +0100

    Comment update.

--- src/CalendarTests.java
+++ src/CalendarTests.java
@@ -9,13 +9,16 @@ public class CalendarTests {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		System.setProperty("webdriver.chrome.driver", "/Users/humanity/Documents/IntelliJ Projects/Selenium Java/Selenium Java/.idea/chromedriver/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/Users/jovanmilosevic/IdeaProjects/chromedriver/chromedriver");
 		WebDriver driver = new ChromeDriver();
 
 		driver.get("https://www.path2usa.com/travel-companion/");
 		driver.findElement(By.id("form-field-travel_comp_date")).click();
 
-		//While loop will keep on executing until it becomes false
+		/**
+		 * While loop will keep on executing until it becomes false. In this case code will keep clicking on
+		 * next button until "January" is displayed.
+		 */
 		while (!driver.findElement(By.className("cur-month")).getText().equalsIgnoreCase("January")) {
 			driver.findElement(By.className("flatpickr-next-month")).click();
 		}
