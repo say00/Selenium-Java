741547b64f2a494610060f04dbf6472475270895
jovanmilosevic <jovanmil87@gmail.com>
2023-12-03 12:49:16 +0100

    Added screenshot FileUtils and getRect method

--- src/NewWindowTests.java
+++ src/NewWindowTests.java
@@ -1,14 +1,15 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WindowType;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
 
 public class NewWindowTests {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
 
         System.setProperty("webdriver.chrome.driver", "/Users/jovanmilosevic/IdeaProjects/chromedriver/chromedriver");
         WebDriver driver = new ChromeDriver();
@@ -16,6 +17,7 @@ public class NewWindowTests {
         driver.get("https://rahulshettyacademy.com/angularpractice/");
         driver.switchTo().newWindow(WindowType.TAB);
 
+        //Switching window
         Set<String> handles = driver.getWindowHandles();
         Iterator<String> it = handles.iterator();
         String parentWindow = it.next();
@@ -25,7 +27,17 @@ public class NewWindowTests {
         String courseName = driver.findElements(By.cssSelector("a[href*='https://courses.rahulshettyacademy.com/p']")).get(1).getText();
 
         driver.switchTo().window(parentWindow);
-        driver.findElement(By.cssSelector(".form-control.ng-untouched.ng-pristine.ng-invalid")).sendKeys(courseName);
+        WebElement name = driver.findElement(By.cssSelector(".form-control.ng-untouched.ng-pristine.ng-invalid"));
+        name.sendKeys(courseName);
+
+        //Screenshot
+        File file = name.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file, new File("logo.png"));
+
+        //Get Height & Width
+        System.out.println(name.getRect().getDimension().getHeight());
+        System.out.println(name.getRect().getDimension().getWidth());
+
 
         Thread.sleep(1500);
         driver.quit();
